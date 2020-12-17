5c6470b936a62007f15c6f78d7bbbc63fb114991
Himanshu Anand <himanshuanand05@gmail.com>
2020-12-17 10:08:41 +0530

    Stable code commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '431744835757-7hpojnoambd2305shj5i26b3e70kkhld.apps.googleusercontent.com',
+            clientSecret: '5Rg5nHDS72P07SBTMoeFRue-'
+        },
+        mongodb: {
+            dbURI: 'mongodb+srv://himu:1234@cluster0.oj5pj.mongodb.net/OAuth?retryWrites=true&w=majority'
+        },
+        session: {
+            cookieKey: 'thenetninjaisawesomeiguess'
+        }
+};
